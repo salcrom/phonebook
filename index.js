b81e801bd3126d49f04f0ca85602b101a8fa8db9
salcrom <seya.28@gmail.com>
2024-12-19 19:47:32 +0100

    phonebook - paso 2 listo

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ app.get("/api/persons", (request, response) => {
     response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+    const person = persons.length;
+    const date = new Date().toString();
+
+    response.send(`<p>Phonebook has info for ${person} people</p>
+        <p>${date}</p>`);
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
